eb5deca106671e8f489ea23c989a36dbe3a622eb
Esteban Arenas <92794098+arenasesteban@users.noreply.github.com>
2026-03-28 01:43:04 -0300

    CHG: compute stats with Counter and include invalid_lines

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,35 +1,45 @@
-import csv
+import logging
+from collections import Counter
 
-from utils import write_txt, write_csv
+from log_entry import LogEntry
 
-def analyze_logs(logs, filter_level):
+logger = logging.getLogger(__name__)
+
+
+def compute_logs_statistics(logs: list[LogEntry], invalid_lines: int, filter_level: str) -> dict:
+    logger.info("Starting to compute log statistics")
+
+    if not logs:
+        logger.warning("No valid log entries found.")
+        return {
+            "total_logs": 0,
+            "invalid_lines": invalid_lines,
+            "levels": {}
+        }
+    
     if filter_level != "ALL":
         logs = filter_logs_by_level(logs, filter_level)
 
-    summary = summarize_logs(logs)
+    summary = count_logs_by_level(logs, invalid_lines)
+
+    logger.info(f"Finished computing log statistics.")
+
     return summary
 
-def filter_logs_by_level(logs, filter_level):
+
+def filter_logs_by_level(logs: list[LogEntry], filter_level: str) -> list[LogEntry]:
     return [log for log in logs if log.log_level == filter_level]
 
 
-def summarize_logs(logs):
+def count_logs_by_level(logs: list[LogEntry], invalid_lines: int) -> dict:
     summary = {
         "total_logs": len(logs),
-        "levels": {}
+        "invalid_lines": invalid_lines,
+        "levels": Counter(log.log_level for log in logs)
     }
 
-    for log in logs:
-        if log.log_level not in summary["levels"]:
-            summary["levels"][log.log_level] = 0
-        
-        summary["levels"][log.log_level] += 1
+    logger.debug(f"Total logs: {summary['total_logs']}")
+    logger.debug(f"Invalid lines: {summary['invalid_lines']}")
+    logger.debug(f"Log levels count: {summary['levels']}")
 
     return summary
-
-def write_summary(summary, output_dir, file_name):
-    file_name = file_name.split(".")[0]
-    output_dir = f"{output_dir}/{file_name}_output"
-
-    write_txt(summary, output_dir)
-    write_csv(summary, output_dir)
